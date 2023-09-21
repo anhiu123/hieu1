11e67667c2a169f36f21c945c24aa09695c8e223
admin <hieundph29367@fpt.edu.vn>
2023-09-21 15:15:03 +0700

    hahaha

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase/app';
+import 'firebase/auth'; // Import các module bạn muốn sử dụng (auth, database, storage, etc.)
 
 export default function App() {
   return (
